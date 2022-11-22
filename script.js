06a1430a6c8e0a8e6fc3cfa0e2983b86e6181f7b
Belter Vásquez <beltervasquezflores@gmail.com>
2022-11-22 13:04:44 -0600

    Problema resuelto el anterior codigo solo funcionaba en firefix y este ya funciona en todos los buscadores

--- script.js
+++ script.js
@@ -1,5 +1,89 @@
-/**Bandera Guatemala */
-function bandera1(){
+let banderaGrupo = document.getElementById('banderasJuntas');
+
+
+function vereficarBandera(elemento){
+    let insertaBanderas = document.getElementById('insertarContenido');
+
+    if(elemento.value == 'banderaGua'){
+        insertaBanderas.innerHTML = `<article id="ejercicio1">
+        <h2>Ejercicio 1: Guatemala</h2>
+        <div class="contenedor">
+            <div class="parte1"></div>
+            <div class="parte3"></div>
+            <div class="parte2"><img src="img/guatemala.png" alt="Escudo Guatemala"></div>
+        </div>
+    </article>`
+    }else if(elemento.value == 'banderaIndia'){
+        insertaBanderas.innerHTML = `<article id="ejercicio2">
+        <h2>Ejercicio 2: Níger, India</h2>
+        <div class="contenedor">
+            <div class="parte1"></div>
+            <div class="parte2"></div>
+            <div class="parte3"></div>
+            <div class="parte4"></div>
+            <div class="parte5"><img src="img/niger.png" alt="Emblema Níger"></div>
+            <div class="parte6"><img src="img/india.png" alt="Emblema India"></div>
+            <div class="parte7"></div>
+        </div>
+    </article>`;
+    }else if(elemento.value == 'banderaba'){
+        insertaBanderas.innerHTML = `<article id="ejercicio3">
+        <h2>Ejercicio 3: Paises Bajos, Francia</h2>
+        <div class="contenedor">
+        <div id="bajos">
+            <div class="parte1"></div>
+            <div class="parte2"></div>
+            <div class="parte3"></div>
+        </div>
+        <div class="francia">
+            <div class="parte4"></div>
+            <div class="parte5"></div>
+            <div class="parte6"></div>
+        </div>
+        <div class="parte7"></div>
+    </div>
+    </article>`;
+    }else if(elemento.value == 'banderaSue'){
+        insertaBanderas.innerHTML = ` <article id="ejercicio4">
+        <h2>Ejercicio 4: Suecia</h2>
+        <div class="contenedor">
+            <div id="superior">
+                <div class="parte1"></div>
+                <div class="parte2"></div>
+                <div class="parte3"></div>
+            </div>
+            <div class="parte4"></div>
+            <div id="inferior">
+                <div class="parte5"></div>
+                <div class="parte6"></div>
+                <div class="parte7"></div>
+            </div>
+        </div>
+    </article>`;
+    }else{
+        insertaBanderas.innerHTML = ` <article id="ejercicio5">
+        <h2>Ejercicio 5: Suiza</h2>
+        <div class="contenedor">
+            <div class="parte1"></div>
+            <div class="parte2"></div>
+            <div class="parte3"></div>
+        </div>
+    </article>`;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+/*function bandera1(){
     let content = ` <article id="ejercicio1">
     <h2>Ejercicio 1: Guatemala</h2>
     <div class="contenedor">
@@ -12,7 +96,8 @@ let insentar = document.getElementById('insertarContenido');
 
 insentar.innerHTML = content;
 }
-/**Bandera de la india */
+/**Bandera de la india 
+
 function bandera2(){
     let content = ` <article id="ejercicio2">
     <h2>Ejercicio 2: Níger, India</h2>
@@ -30,7 +115,8 @@ let insentar = document.getElementById('insertarContenido');
 
 insentar.innerHTML = content;
 }
-/**Bandera Paises bajos*/
+/**Bandera Paises bajos
+
 function bandera3(){
     let content = `<article id="ejercicio3">
     <h2>Ejercicio 3: Paises Bajos, Francia</h2>
@@ -52,6 +138,7 @@ let insentar = document.getElementById('insertarContenido');
 
 insentar.innerHTML = content;
 }
+
 function bandera4(){
     let content = ` <article id="ejercicio4">
     <h2>Ejercicio 4: Suecia</h2>
@@ -86,4 +173,4 @@ function bandera5(){
 let insentar = document.getElementById('insertarContenido');
 
 insentar.innerHTML = content;
-}
+}*/
